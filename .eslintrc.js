dcdd343f8f5cf158904ff47367a3b2bc23b46dbd
Jon Cass <joncass@gmail.com>
2017-05-27 19:45:05 -0400

    fix indentation in a couple of files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,21 @@
 module.exports = {
-    "extends": "google",
-    "installedESLint": true,
-    "parserOptions": {
-        "ecmaVersion": 6
-    },
-    "rules": {
-        "brace-style": ["error", "stroustrup"],
-        "guard-for-in": "off",
-        "indent": ["error", 2],
-        "linebreak-style": ["error", "unix"],
-        "max-len": "off",
-        "max-statements-per-line": ["error", { "max": 2 }],
-        "no-console": "off",
-        "no-warning-comments": "off",
-        "object-curly-spacing": ["error", "always"],
-        "padded-blocks": "off",
-        "prefer-const": ["error"],
-        "require-jsdoc": "off"
-    }
-
+  "extends": "google",
+  "installedESLint": true,
+  "parserOptions": {
+    "ecmaVersion": 6
+  },
+  "rules": {
+    "brace-style": ["error", "stroustrup"],
+    "guard-for-in": "off",
+    "indent": ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    "max-len": "off",
+    "max-statements-per-line": ["error", { "max": 2 }],
+    "no-console": "off",
+    "no-warning-comments": "off",
+    "object-curly-spacing": ["error", "always"],
+    "padded-blocks": "off",
+    "prefer-const": ["error"],
+    "require-jsdoc": "off"
+  }
 };
