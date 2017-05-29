a73707051c0e08e35baf7ab0954439756fcb6cea
Jon Cass <joncass@gmail.com>
2017-05-29 11:13:13 -0400

    allow a player to catch the disc at which point the player and disc stop moving

--- src/common/UltimateGame.js
+++ src/common/UltimateGame.js
@@ -60,7 +60,7 @@ class UltimateGame extends GameEngine {
 
     // get the player tied to the player socket
     const player = this.world.getPlayerObject(playerId);
-    if (player) {
+    if (player && !player.hasDisc) {
       if (
         inputData.input === 'up'
         && player.position.y > 0
@@ -94,49 +94,41 @@ class UltimateGame extends GameEngine {
       return;
     }
 
-    // CHECK LEFT EDGE:
     if (
-      this.disc.position.x <= PADDING + PLAYER_WIDTH
-      && this.disc.position.y >= this.player1.y
-      && this.disc.position.y <= this.player1.position.y + PLAYER_HEIGHT
-      && this.disc.velocity.x < 0
+      Math.abs(this.disc.position.x - this.player1.x) <= 10
+      && Math.abs(this.disc.position.y - this.player1.y) <= 10
     ) {
-      // disc moving left hit player 1 player
-      this.disc.velocity.x *= -1;
-      this.disc.position.x = PADDING + PLAYER_WIDTH + 1;
+      this.disc.velocity.x = 0;
+      this.disc.velocity.y = 0;
+      this.player1.hasDisc = true;
     }
-    else if (this.disc.position.x <= 0) {
-      // disc hit left wall
+    else if (
+      Math.abs(this.disc.position.x - this.player2.x) <= 10
+      && Math.abs(this.disc.position.y - this.player2.y) <= 10
+    ) {
+      this.disc.velocity.x = 0;
+      this.disc.velocity.y = 0;
+      this.player2.hasDisc = true;
+    }
+
+    // Bounce if the disc hits an edge
+    if (this.disc.position.x <= 0) {
+      // Left wall
       this.disc.velocity.x *= -1;
       this.disc.position.x = 0;
-      console.log(`player 2 scored`);
     }
-
-    // CHECK RIGHT EDGE:
-    if (
-      this.disc.position.x >= WIDTH - PADDING - PLAYER_WIDTH
-      && this.disc.position.y >= this.player2.position.y
-      && this.disc.position.y <= this.player2.position.y + PLAYER_HEIGHT
-      && this.disc.velocity.x > 0
-    ) {
-      // disc moving right hits player 2 player
-      this.disc.velocity.x *= -1;
-      this.disc.position.x = WIDTH - PADDING - PLAYER_WIDTH - 1;
-    }
-    else if (this.disc.position.x >= WIDTH ) {
-      // disc hit right wall
+    if (this.disc.position.x >= WIDTH ) {
+      // Right wall
       this.disc.velocity.x *= -1;
       this.disc.position.x = WIDTH - 1;
-      console.log(`player 1 scored`);
     }
-
-    // disc hits top
     if (this.disc.position.y <= 0) {
+      // Top
       this.disc.position.y = 1;
       this.disc.velocity.y *= -1;
     }
     else if (this.disc.position.y >= HEIGHT) {
-      // disc hits bottom
+      // Bottom
       this.disc.position.y = HEIGHT - 1;
       this.disc.velocity.y *= -1;
     }
